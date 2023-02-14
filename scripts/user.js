1d59f2752b2d1cb1a9955b58b018e09f1005b0e8
Altasa <mehanu.kea@gmail.com>
2023-02-14 19:00:22 +0700

    Update 006

--- scripts/user.js
+++ scripts/user.js
@@ -11,15 +11,25 @@ sign_in.addEventListener('click', SignIn);
 function SignUp(){
     var username=login.value;
     var password=passw.value;
+    alert(username+" "+password);
 }
 sign_up.addEventListener('click', SignUp);
-//var requestURL="https://altasa.github.io/SPTP/data.json";
-var requestURL="../data.json";
+//database
+var requestURL="https://altasa.github.io/SPTP";
+//try load file
 var request=new XMLHttpRequest();
-request.open('GET', requestURL);
+request.open('GET', requestURL+"/data.json");
 request.responseType="json";
 request.send();
 request.onload=function(){
     var database=request.response;
     alert(database['name']);
 }
+//try save file
+var registr=new XMLHttpRequest();
+registr.open('POST', requestURL);
+registr.setRequestHeader('Content-Type', 'application/json charset=utf-8');
+let json=JSON.stringify(
+    {"name":"Example"}
+);
+registr.send(json);
